34bb5dd211c0966b9f04cc1cf8e486c5bc8e4eed
OlivierCookie <olivier.gombert@adeo.com>
2022-06-16 14:53:04 +0200

    deb exo3

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,9 @@
+package com.h2;
+
+public class MortgageCalculator {
+
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+}
